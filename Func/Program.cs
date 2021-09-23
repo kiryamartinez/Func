5f1aebbca5f99492e6c504ddaec61564634ba6f5
Kirya Martinez <kirilltitov-n@mail.ru>
2021-09-23 20:37:34 +0600

    Функция рассширения массивов

--- Func/Program.cs
+++ Func/Program.cs
@@ -10,15 +10,35 @@ namespace Func
     {
         static void Main(string[] args)
         {
-            int[] array = new int[0];
-            array = EditArray(array, 10, 2, 5);
-            Console.WriteLine(array[2]);
+            int[] array = new int[5];
+            int[,] array2 = new int[1, 1];
+            array = ResizeArray(array, 20);
+            Console.WriteLine(array.Length);
+            array2 = ResizeArray(array2, 4, 10);
+            Console.WriteLine(array2.GetLength(0) + " " + array2.GetLength(1));
         }
 
-        static int[] EditArray(int[] array, int sizeArray, int index , int value)
+        static int[] ResizeArray(int[] array, int size)
         {
-            array = new int[sizeArray];
-            array[index] = value;
+            int[] tempArray = new int[size];
+            for (int i = 0 ; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            array = tempArray;
+            return array;
+        }
+        static int[,] ResizeArray(int[,] array, int sizeX, int sizeY)
+        {
+            int[,] tempArray = new int[sizeX,sizeY];
+            for (int i = 0; i<array.GetLength(0); i++ )
+            {
+                for (int j = 0; j<array.GetLength(1); j++) 
+                {
+                    tempArray[i, j] = array[i, j];
+                }
+            }
+            array = tempArray;
             return array;
         }
     }
